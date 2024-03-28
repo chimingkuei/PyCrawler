b2ee86dd2c30a86624c7565b6ab769733b07a895
chimingkuei <chimingkuei@gmail.com>
2024-03-28 21:25:51 +0800

    PyCrawler

--- Crawler.py
+++ Crawler.py
@@ -45,6 +45,14 @@ class Crawler:
         # 将DataFrame写入Excel文件
         with pd.ExcelWriter(self.ouputexcel_path, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
             df.to_excel(writer, sheet_name=sheet_name, startrow=start_row-1, index=False, header=False)
+     
+    def catch_element(self, soup, element):
+        result = soup.find("td", string=element)
+        if result is not None:
+            data = result.find_next_sibling("td").get_text(strip=True)
+        else:
+            data = ""
+        return data
 
     def GrabCompanyInfo(self, company):
         if self.CheckInternet():
@@ -61,43 +69,58 @@ class Crawler:
             search_box.send_keys(company)
             search_box.send_keys(Keys.RETURN)
             time.sleep(3)
-            search_link = driver.find_element(By.LINK_TEXT, company)
-            search_link.click()
-            time.sleep(3)
-            html_content = driver.page_source
-            driver.close()
-            # 儲存整個網頁
-            # with open('saved_page.html', 'w', encoding='utf-8') as file:
-            #     file.write(html_content)
-            soup = BeautifulSoup(html_content, 'html.parser')
-            # 印出整個網頁
-            #print(soup.prettify())
-            # 印出網頁部分資訊
-            VAT_number = soup.find("td", string="統一編號").find_next_sibling("td").get_text(strip=True).rstrip('訂閱')
-            company_name = soup.find("td", string="公司名稱").find_next_sibling("td").get_text(strip=True).split('公司')[0]+"公司"
-            capital = soup.find("td", string="資本總額(元)").find_next_sibling("td").get_text(strip=True)
-            actual_capital = soup.find("td", string="實收資本額(元)").find_next_sibling("td").get_text(strip=True)
-            representative_name = soup.find("td", string="代表人姓名").find_next_sibling("td").get_text(strip=True)
-            approved_date_of_establishment = soup.find("td", string="核准設立日期").find_next_sibling("td").get_text(strip=True)
-            business_information = soup.find("td", string="所營事業資料").find_next_sibling("td").get_text(strip=True)
-            print("統一編號:", VAT_number)
-            print("公司名稱:", company_name)
-            print("資本總額(元):", capital)
-            print("實收資本額(元):", actual_capital)
-            print("代表人姓名:", representative_name)
-            print("核准設立日期:",  approved_date_of_establishment)
-            print("所營事業資料:", business_information)
-            time.sleep(5)
-            data = {
-                '統一編號': [VAT_number],
-                '公司名稱': [company_name],
-                '資本總額(元)': [capital],
-                '實收資本額(元)': [actual_capital],
-                '代表人姓名': [representative_name],
-                '核准設立日期': [approved_date_of_establishment],
-                '所營事業資料': [business_information]
-            }
-            self.WriteExcel(data)
+            try:
+                search_link = driver.find_element(By.LINK_TEXT, company)
+                search_link.click()
+                time.sleep(3)
+                html_content = driver.page_source
+                driver.close()
+                # 儲存整個網頁
+                # with open('saved_page.html', 'w', encoding='utf-8') as file:
+                #     file.write(html_content)
+                soup = BeautifulSoup(html_content, 'html.parser')
+                # 印出整個網頁
+                #print(soup.prettify())
+                # 印出網頁部分資訊
+                data=""
+                if 	"解散" in self.catch_element(soup, "公司狀況"):
+                    company_name= self.catch_element(soup, "公司名稱").split('公司')[0]+"公司"
+                    data = {
+                        '統一編號': ['解散'],
+                        '公司名稱': [company_name]
+                    }
+                else:
+                    VAT_number= self.catch_element(soup, "統一編號").rstrip('訂閱')
+                    company_name= self.catch_element(soup, "公司名稱").split('公司')[0]+"公司"
+                    capital= self.catch_element(soup, "資本總額(元)")
+                    actual_capital= self.catch_element(soup, "實收資本額(元)")
+                    representative_name = self.catch_element(soup, "代表人姓名")
+                    approved_date_of_establishment = self.catch_element(soup, "核准設立日期")
+                    business_information = self.catch_element(soup, "所營事業資料")
+                    print("統一編號:", VAT_number)
+                    print("公司名稱:", company_name)
+                    print("資本總額(元):", capital)
+                    print("實收資本額(元):", actual_capital)
+                    print("代表人姓名:", representative_name)
+                    print("核准設立日期:",  approved_date_of_establishment)
+                    print("所營事業資料:", business_information)
+                    time.sleep(5)
+                    data = {
+                        '統一編號': [VAT_number],
+                        '公司名稱': [company_name],
+                        '核准設立日期': [approved_date_of_establishment],
+                        '資本總額(元)': [capital],
+                        '實收資本額(元)': [actual_capital],
+                        '代表人姓名': [representative_name],
+                        '所營事業資料': [business_information]
+                    }
+                self.WriteExcel(data)
+            except:
+                data = {
+                        '統一編號': ['找不到'],
+                        '公司名稱': [company],
+                    }
+                self.WriteExcel(data)
         else:
             print("請確認網路連線!")
 
@@ -108,5 +131,5 @@ class Crawler:
 
 
 if __name__ == '__main__':
-    Object = Crawler(r"D:\Chimingkuei\Python\PyCrawler\Data.xlsx", "output.xlsx")
+    Object = Crawler("Data.xlsx", "output.xlsx")
     Object.BatchGrabCompanyInfo()
